d2dccaf8e78fcbe80b8a5fe2b3fa476ac25444de
unknown <pendarvisjc@gmail.com>
2020-01-07 10:34:41 -0500

    Pages Added

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+  <meta charset='utf-8'>
+  <meta name='description' content='Portfolio for real estate agent, Krystal Queen'>
+  <meta name='viewport' content='width=device-width, intial-scale=1.0'>
+  <meta name='author' content='Jonathan Pendarvis'>
+  <title>Queen Realty</title>
+
+  <!-- CSS -->
+  <link rel='stylesheet' href='stylesheets/public.css'>
+
+</head>
+
+<body>
+
+  <header>
+    <h1 id='h1.queen'>Queen Realty</h1>
+
+    <navigation>
+      <ul id="menu">
+        <li><a href='index.php'>Home</a></li>
+        <li><a href='listings.php'>Listings</a></li>
+        <li><a href='about.php'>About Me</a></li>
+        <li><a href='contact.php'>Contact</a></li>
+      </ul>
+    </navigation>
+
+  </header>
+
+
+</body>
